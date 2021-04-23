0dde3422da6ab605fed6c1e0194cccf175eebfb4
murshidp <murshidparambadan@gmail.com>
2021-04-23 17:52:07 +0530

    template rendering

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template, url_for
 app = Flask(__name__)
+
+posts =  [
+    {
+        'author' : "murshid",
+        'title' : 'blog post 1',
+        'content' : "first blog content",
+        'date' : "20-04-2020"
+    },
+      {
+        'author' : "sam",
+        'title' : 'blog post 2',
+        'content' : "second blog content",
+        'date' : "20-03-2020"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>hello world!<h1>"
+    return render_template("home.html",posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>about as</h1>"
+    return render_template("about.html",title="about")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- readme.md
+++ readme.md
@@ -3,3 +3,10 @@
 * seting environmental variable
     * export FLASK_APP=main.py 
     * export FLASK_DEBUG=1
+
+
+### 2. Templates
+ * to render template: from flask import render_template
+ * to use url : from flask import url_for
+ * using url in html :
+    *   <link rel="stylesheet" type="text/css" href="{{ url_for('static', filename='main.css') }}" />
